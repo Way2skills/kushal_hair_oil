472b4b008333c487d6378a3f95c2d317fcb6f7aa
Sachinn-p <128300170+Sachinn-p@users.noreply.github.com>
2025-04-12 20:36:40 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
-import type { Metadata } from 'next'
-import './globals.css'
+// app/layout.tsx
+
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'KOWSHI HERBAL HAIR OIL',
-  description: 'We source all our ingredients locally from organic farms to ensure the highest quality. Each batch is prepared with care, following time-tested methods that preserve the natural goodness of herbs.',
-  
-}
+  title: 'KOWSHI HERBAL HAIR OIL',
+  description:
+    'We source all our ingredients locally from organic farms to ensure the highest quality. Each batch is prepared with care, following time-tested methods that preserve the natural goodness of herbs.',
+  verification: {
+    google: '-EcOyQsbDOxHvLcSQi8f_MRZ-z_BsQNOQA4SNFEovg0',
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="google-site-verification" content="-EcOyQsbDOxHvLcSQi8f_MRZ-z_BsQNOQA4SNFEovg0" />
-      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
